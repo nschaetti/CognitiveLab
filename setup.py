f6d1e718e7c2164caac603c27783fc416641ad86
CL-5NZZ473\nschaetti <nils.schaetti@unige.ch>
2020-10-30 15:12:24 +0100

    Initial push

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CognitiveLab',
+    version='1.0',
+    packages=['cognitivelab'],
+    url='https://github.com/nschaetti/CognitiveLab',
+    license='GPL v3',
+    author='nschaetti',
+    author_email='nils.schaetti@unige.ch',
+    description='Manage and Analyse Output of Machine Learning Experiments'
+)
